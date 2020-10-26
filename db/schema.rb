7e0bac00f8e02041a46b0459062a41fd00a76153
Jeremy Oliver <Jeremy@Jeremys-MacBook-Pro.local>
2020-10-25 18:01:25 -0600

    added signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_021038) do
+ActiveRecord::Schema.define(version: 2020_10_25_232635) do
 
   create_table "courses", force: :cascade do |t|
     t.integer "department_id", null: false
@@ -66,6 +66,18 @@ ActiveRecord::Schema.define(version: 2020_09_28_021038) do
     t.integer "identifier"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "courses", "departments"
